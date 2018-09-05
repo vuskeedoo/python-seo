4afd13df52308a563cf363f51db23e0a61fdbd71
Kevin <vu.kevin00@gmail.com>
2018-09-05 13:14:55 -0700

    added similar keywords function

--- searchkey.py
+++ searchkey.py
@@ -1,11 +1,11 @@
 from client import RestClient
-#You can download this file from here https://api.dataforseo.com/_examples/python/_python_Client.zip
 
-#Instead of 'login' and 'password' use your credentials from https://my.dataforseo.com/login
 client = RestClient('vu.kevin@csu.fullerton.edu', 'LD87rm8Od9dfWnVn')
 
+# Search class has modules for different functions
 class Search():
-    def stats(self, word, location):
+    # https://docs.dataforseo.com/#live-data
+    def volume(self, word, location):
         keywords_list = [
             dict(
                 language="en",
@@ -20,6 +20,7 @@ class Search():
         else:
             print(response["results"])
 
+    # https://docs.dataforseo.com/#live-data53
     def keywords(self, word, location):
         keywords_list = [
             dict(
@@ -29,6 +30,36 @@ class Search():
             )
         ]
         response = client.post("/v2/kwrd_for_keywords", dict(data=keywords_list))
+        if response["status"] == "error":
+            print("error. Code: %d Message: %s" % (response["error"]["code"], response["error"]["message"]))
+        else:
+            print(response["results"])
+
+    # https://docs.dataforseo.com/#get-related-keywords
+    def similar_keywords(self, word, location):
+        rnd = Random() #you can set as "index of post_data" your ID, string, etc. we will return it with all results.
+        post_data = dict()
+
+        post_data[rnd.randint(1, 30000000)] = dict(
+            keyword=word,
+            country_code="US",
+            language="en",
+            depth=2,
+            limit=1,
+            offset=0,
+            orderby="cpc,desc",
+            filters=[
+                ["cpc", ">", 0],
+                "or", 
+                [
+                    ["search_volume", ">", 0],
+                    "and",
+                    ["search_volume", "<=", 1000]
+                ]
+            ]
+        )
+
+        response = client.post("/v2/kwrd_finder_related_keywords_get", dict(data=post_data))
         if response["status"] == "error":
             print("error. Code: %d Message: %s" % (response["error"]["code"], response["error"]["message"]))
         else:
