e3331c44543804c45aab55b3b182c93a2e569857
liamfitz85 <liam.fitz85@yahoo.com>
2018-09-29 11:39:06 -0700

    [issue 02]

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from restapi import Search, Trumpia
+import pprint
 
 # initialize object from searchkey.py
 api = Search()
@@ -7,12 +8,13 @@ trm = Trumpia()
 keys = ['spiderman']
 
 # example functions
-results_k = api.keywords(keys, 'United States')
-print(results_k[1])
-print(len(results_k))
-print('-----------------')
-#results_kr = api.keywords_related('waffles','US')
-#print(results_kr)
-#print('-------')
+# results_k = api.keywords(keys, 'United States')
+#print(results_k[1])
+#print(len(results_k))
+#print('-----------------')
+results_kr = api.keywords_related(keys[0],'US')
+# print(results_kr)
+pprint(results_kr)
+# print('-------')
 # send email
 #trm.sendEmail('vu.kevin00@gmail.com', 'body message', 'subject')
