b6d775a4961adc45d8d34a376f4d166399cb1ee3
liamfitz85 <liam.fitz85@yahoo.com>
2018-09-29 15:11:43 -0700

    [issue 05] sweeeeeet

--- parsing-json.py
+++ parsing-json.py
@@ -1,9 +1,15 @@
 import json
 
-dicti = json.load(open('spiderman.json'))
+data = json.loads(open('spiderman.json').read())
 
-print(dicti["29771621"]["related"][1])
+#print(dicti["29771621"]["related"][1])
 
-print("\n\n\n\n")
+#print("\n\n\n\n")
 
-print(dicti[next(iter(dicti))]["related"][0])
+#print(dicti[next(iter(dicti))]["related"][0])
+
+#root_response = data[next(iter(data))]
+
+#print(root_response)
+
+print(json.dumps(data, indent=2, sort_keys=False))
