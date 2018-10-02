d7e2eb94b56d2635934bb53e597c52478481fd21
liamfitz85 <liam.fitz85@yahoo.com>
2018-10-01 18:05:19 -0700

    [working] use file, not API

--- parsing-json.py
+++ parsing-json.py
@@ -1,46 +1,18 @@
 import json
 from restapi import Search
 
-"""
+'''
 response = Search()
 
+# these three lines create an API call
 data = response.keywords_related('superman','US')
-
 data = str(data).replace("'","\"")
-
 data = json.loads(data)
-"""
+'''
 
+# this line uses a file so that we do not use credits while debugging, etc.
 data = json.loads(open('superman.json').read())
-
-print(type(data))
-
-#print(dicti["29771621"]["related"][1])
-
-#print("\n\n\n\n")
-
-#print(dicti[next(iter(dicti))]["related"][0])
-
-#root_response = data[next(iter(data))]
-
-#print(root_response)
-
-
-#print( data )
-#print(type(data))
-
-#print(json.dumps(data, indent=2, sort_keys=False))
-
-#print("\n\n\n")
-
-'''
-for k in data[next(iter(data))]:
-	k = dict(k)
-	print(k["related"])
-	print("\n")
-'''
-
-#print( data[next(iter(data))]["related"][0] )
 
 for v in data[next(iter(data))]["related"]:
 	print(v["key"])
+	print(v["country_code"])
