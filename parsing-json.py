fe4bb32789e684dc05127ef227938313ba914927
liamfitz85 <liam.fitz85@yahoo.com>
2018-10-04 09:48:43 -0700

    initial commit for sql

--- parsing-json.py
+++ parsing-json.py
@@ -1,3 +1,4 @@
+"""
 import json
 from restapi import Search
 
@@ -16,3 +17,30 @@ data = json.loads(open('superman.json').read())
 for v in data[next(iter(data))]["related"]:
 	print(v["key"])
 	print(v["country_code"])
+"""
+import json
+from restapi import Search
+
+class JsonResponse():
+
+	def json_response(self):
+		search = Search()
+		json_data = search.keywords_related('the hulk','US')
+		json_data = str(json_data).replace("'","\"")
+		json_data = json.loads(json_data)
+		return json_data
+
+	def pretty_print(self, json_data):
+		self.json_data = json_data
+		print(json.dumps(json_data, sort_keys=False, indent=2))
+
+	def debug(self, json_data):
+		return
+
+	def sql_string(self, json_data):
+		self.json_data = json_data
+		return
+
+json_response = JsonResponse()
+json_search = json_response.json_response()
+json_response.pretty_print(json_search)
