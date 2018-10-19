411d6c6ad78a3b865c0111475b91296ed6e31bdf
liamfitz85 <liam.fitz85@yahoo.com>
2018-10-18 21:43:51 -0700

    removed unnecessary loop

--- test_db.py
+++ test_db.py
@@ -4,12 +4,11 @@ from parsing_json import JsonResponse
 sql = SQLString()
 response = JsonResponse()
 
-json_search = response.json_response("aquaman", "US")
+json_search = response.json_response("black ops iiii", "US")
 
 print("Searched for: %s" % sql.get_search_key(json_search))
 
-for data in json_search[next(iter(json_search))]["related"]:
-	sql_insert_tuple = sql.get_insert_tuple(json_search)
+sql_insert_tuple = sql.get_insert_tuple(json_search)
 
 for tup in sql_insert_tuple:
 	insert_string = sql.insert_sql_string(tup)
